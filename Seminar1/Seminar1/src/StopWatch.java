17a50519a4686f12194bfc9a89e9c6b569000d2c
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2023-09-07 20:39:19 +0200

    seminar 1 done

--- Seminar1/Seminar1/src/StopWatch.java
+++ Seminar1/Seminar1/src/StopWatch.java
@@ -11,26 +11,6 @@ public class StopWatch implements Runnable{
 
     @Override
     public void run() {
-        double elapsedTime = 0; //start time
-        while (true){
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            elapsedTime = elapsedTime + 0.01; // add a millisecond for every iteration
-            String formattedString = String.format("%.2f", elapsedTime); // shows the result with only two digits
-            System.out.println(formattedString);
-
-            if(elapsedTime >= 4.99){
-                break;
-            }
-
-        }
-    }
-
-    /*@Override
-    public void run() {
         System.out.println("Stopwatch thread. Stopwatch starts now!");
         long startTime = System.currentTimeMillis();
         while (true){
@@ -44,12 +24,46 @@ public class StopWatch implements Runnable{
             String formattedString = String.format("%.2f", elapsedTime); // shows the result with only two digits
             System.out.println("Stopwatch thread. Elapsed: " + formattedString);
 
-            if(elapsedTime >= 5){
+            if(elapsedTime > 60){
                 break;
             }
         }
-    }*/
+    }
 }
+
+/*  1. What does the Thread.join() do? What happens if we remove it from the main method?
+        Thread.join makes the threads wait for each other to be excuted.
+    2. Compare the real elapsed time using a stopwatch on your phone with your Java implementation. Does the elapsed time differ, why?
+        It depends on the implamention of the stopWatch. If you use the System.currentTimeMillis it will not differ,
+        but if you try to do manually it will differ a lot
+ */
+
+
+
+    // Another way
+    /*@Override
+    public void run() {
+        System.out.println("Stopwatch thread. Stopwatch starts now!");
+        double elapsedTime = 0; //start time
+        while (true){
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            elapsedTime = elapsedTime + 0.01; // add a millisecond for every iteration
+            String formattedString = String.format("%.2f", elapsedTime); // shows the result with only two digits
+            System.out.println(formattedString);
+
+            if(elapsedTime >= 59.99){
+                break;
+            }
+
+        }
+    }*/
+
+    // Second example
+
 
 /* How did you implement the task?
         Shown in code above
